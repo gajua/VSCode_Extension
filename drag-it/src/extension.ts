18375c213263b9ff5e499f27e494277ed3a5b204
sewon kim <sewon0325@naver.com>
2023-03-13 01:17:48 +0900

    openAI.ts

--- drag-it/src/extension.ts
+++ drag-it/src/extension.ts
@@ -1,43 +1,8 @@
 import * as vscode from 'vscode';
 require('dotenv').config();
 
-const apiKey = process.env.OPENAI_API_KEY;
+
 export function activate(context: vscode.ExtensionContext) {
 
 }
 
-// chat gpt가 알려준 OpenAI API 호출하는 코드 예시
-// import axios from 'axios';
-
-// const OPENAI_API_KEY = 'YOUR_API_KEY';
-
-// async function callOpenAI(endpoint: string, data: any) {
-//   const response = await axios.post(
-//     `https://api.openai.com/v1/${endpoint}`,
-//     data,
-//     {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${OPENAI_API_KEY}`,
-//       },
-//     }
-//   );
-
-//   return response.data;
-// }
-
-// async function exampleUsage() {
-//   const prompt = 'The meaning of life is';
-//   const model = 'text-davinci-002';
-//   const maxTokens = 5;
-
-//   const response = await callOpenAI('completions', {
-//     prompt,
-//     model,
-//     max_tokens: maxTokens,
-//   });
-
-//   console.log(response.choices[0].text);
-// }
-
-// exampleUsage();
