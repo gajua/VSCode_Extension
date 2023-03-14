466e80368fd044916a2e9a000d4171826046e742
sewon kim <sewon0325@naver.com>
2023-03-15 02:49:22 +0900

    에디터 language id 받아오기

--- drag-it/src/extension.ts
+++ drag-it/src/extension.ts
@@ -47,7 +47,7 @@ export const generateCode = async (
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
-    'extension.generateCode',
+    'drag-it.generateCode',
     async () => {
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
@@ -58,19 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
       const { selection } = editor;
       const prompt = editor.document.getText(selection);
 
-      const languageId = editor.document.languageId;
-      let language = '';
-      switch (languageId) {
-        case 'typescript':
-        case 'javascript':
-          language = 'javascript';
-          break;
-        // 추가적인 언어 모드를 처리하고자 할 경우, case를 추가해줄 수 있습니다.
-        default:
-          vscode.window.showErrorMessage(`Unsupported language: ${languageId}`);
-          return;
-      }
-
+      const language = editor.document.languageId;
       const generatedCode = await generateCode(prompt, language);
 
       editor.edit((editBuilder) => {
