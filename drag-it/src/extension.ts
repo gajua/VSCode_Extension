fb11ad951720f42b1ee701683fa82459bbd36e4d
sewon kim <sewon0325@naver.com>
2023-04-05 01:49:47 +0900

    chatGPT.ts 모듈화, apikey 오류

--- drag-it/src/extension.ts
+++ drag-it/src/extension.ts
@@ -1,13 +1,12 @@
 import * as vscode from 'vscode';
 import axios from 'axios';
+import { chatGPT } from './chatGPT';
 require('dotenv').config(); // .env 파일 로드하기
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "sewon-test" is now active!');
-
   let disposable = vscode.commands.registerCommand(
     'drag-it.generateCode',
-    () => {
+    async () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
       // ----------------------
@@ -43,42 +42,19 @@ export function activate(context: vscode.ExtensionContext) {
         return; // no selection
       }
 
-      const selectedTexts = [];
       for (const selection of selections) {
         const selectedText = editor.document.getText(selection);
         vscode.window.showInformationMessage(selectedText);
-        selectedTexts.push(selectedText);
       }
 
       // Send selected text to OpenAI API
-      const openAIEndpoint = 'https://api.openai.com/v1/chat/completions';
-      const openAIKey = process.env.OPENAI_API_KEY;
-      const prompt = selectedTexts.join('\n');
-      const data = {
-        prompt,
-        max_tokens: 1024,
-        n: 1,
-        stop: ['\n'],
-      };
-      const headers = {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${openAIKey}`,
-      };
-      axios
-        .post(openAIEndpoint, data, { headers })
-        .then((response) => {
-          const resultText = response.data.choices[0].text;
-          // 에디터에 결과 추가하기
-          editor.edit((editBuilder) => {
-            // 커서 위치 가져오기
-            const position = editor.selection.active;
-            // 커서 위치에 결과 추가하기
-            editBuilder.insert(position, resultText);
-          });
-        })
-        .catch((error) => {
-          vscode.window.showInformationMessage(error);
-        });
+      const selectedTexts = editor.document.getText(editor.selection);
+      const message = [['user', selectedTexts]];
+      const parameters = { max_tokens: 1024, n: 1, stop: ['\n'] };
+      const response = await chatGPT(message, parameters);
+      editor.edit((editBuilder) => {
+        editBuilder.insert(editor.selection.active, response);
+      });
     }
   );
 
