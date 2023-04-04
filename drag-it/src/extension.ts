7401eeb07469073189d93acc0f6b534cc3049085
sewon kim <sewon0325@naver.com>
2023-04-04 21:55:55 +0900

    dotenv, 에디터에 결과 추가(반응없음)

--- drag-it/src/extension.ts
+++ drag-it/src/extension.ts
@@ -1,20 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import axios from "axios";
+import * as vscode from 'vscode';
+import axios from 'axios';
+require('dotenv').config(); // .env 파일 로드하기
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "sewon-test" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "drag-it.generateCode",
+    'drag-it.generateCode',
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
@@ -41,48 +33,52 @@ export function activate(context: vscode.ExtensionContext) {
       // ------------------------------------
       const editor = vscode.window.activeTextEditor;
       if (!editor) {
-        vscode.window.showInformationMessage(" no active editor");
+        vscode.window.showInformationMessage(' no active editor');
         return; // no active editor
       }
 
       const selections = editor.selections;
       if (selections.length === 0) {
-        vscode.window.showInformationMessage(" no selection");
+        vscode.window.showInformationMessage(' no selection');
         return; // no selection
       }
-      
+
       const selectedTexts = [];
       for (const selection of selections) {
         const selectedText = editor.document.getText(selection);
         vscode.window.showInformationMessage(selectedText);
-        // selectedTexts.push(selectedText);
+        selectedTexts.push(selectedText);
       }
 
       // Send selected text to OpenAI API
-      // const openAIEndpoint =
-      //   "https://api.openai.com/v1/engines/davinci-codex/completions";
-      // const openAIKey = "/"; // replace with your own API key
-      // const prompt = selectedTexts.join("\n");
-      // const data = {
-      //   prompt,
-      //   max_tokens: 1024,
-      //   n: 1,
-      //   stop: ["\n"],
-      // };
-      // const headers = {
-      //   "Content-Type": "application/json",
-      //   Authorization: `Bearer ${openAIKey}`,
-      // };
-      // axios
-      //   .post(openAIEndpoint, data, { headers })
-      //   .then((response) => {
-      //     vscode.window.showInformationMessage(response.data.choices[0].text);
-      //     // console.log(response.data.choices[0].text);
-      //   })
-      //   .catch((error) => {
-      //     // console.error(error);
-      //     vscode.window.showInformationMessage(error);
-      //   });
+      const openAIEndpoint = 'https://api.openai.com/v1/chat/completions';
+      const openAIKey = process.env.OPENAI_API_KEY;
+      const prompt = selectedTexts.join('\n');
+      const data = {
+        prompt,
+        max_tokens: 1024,
+        n: 1,
+        stop: ['\n'],
+      };
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${openAIKey}`,
+      };
+      axios
+        .post(openAIEndpoint, data, { headers })
+        .then((response) => {
+          const resultText = response.data.choices[0].text;
+          // 에디터에 결과 추가하기
+          editor.edit((editBuilder) => {
+            // 커서 위치 가져오기
+            const position = editor.selection.active;
+            // 커서 위치에 결과 추가하기
+            editBuilder.insert(position, resultText);
+          });
+        })
+        .catch((error) => {
+          vscode.window.showInformationMessage(error);
+        });
     }
   );
 
